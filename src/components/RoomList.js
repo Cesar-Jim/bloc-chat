234a64aaf3f8f92eab1ba2efe7f3b37b0f5a14e2
Cesar Jimenez <cijgr@hotmail.com>
2018-11-28 10:17:01 -0600

    Input field now clears after creation of a new room

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,68 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class RoomList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rooms: []
-        }; 
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: []
+    };
 
-        this.roomsRef = this.props.firebase.database().ref('rooms'); // firebase reference
+    this.roomsRef = this.props.firebase.database().ref("rooms"); // firebase reference
+  } // constructor
 
-    } // constructor
+  //componentDidMount method
+  componentDidMount() {
+    this.roomsRef.on("child_added", snapshot => {
+      const room = snapshot.val();
 
+      room.key = snapshot.key;
+      this.setState({
+        rooms: this.state.rooms.concat(room)
+      });
+    });
+  }
 
-    //componentDidMount method
-    componentDidMount() {  
+  // createRoom method
+  createRoom(e) {
+    if (e) e.preventDefault();
+    const newRoomName = this.refs.roomName.value;
+    this.roomsRef.push({
+      name: newRoomName
+    });
 
-        this.roomsRef.on('child_added', snapshot => {
-            const room = snapshot.val();
-            
-            room.key = snapshot.key;
-            this.setState({
-                rooms: this.state.rooms.concat( room )
-            });
+    this.refs.roomName.value = ""; // clearing the input field after room was created.
+  }
 
-        });
-        
-    }
-
-
-    // createRoom method
-    createRoom(e) {
-        if(e) e.preventDefault();
-        const newRoomName = this.refs.roomName.value;
-        this.roomsRef.push({
-            name: newRoomName
-        })
-    }
-
-
-    render() {
-        return (
-            <div>
-                <h1>Available Rooms:</h1>
-                {this.state.rooms.map( (room, i) => 
-                <div>
-                    <h4 className="room-name" key={i.toString()}>
-                        {room.name}
-                    </h4>
-                </div>
-                )}
-                <div>
-                    <form onSubmit={(e) => this.createRoom(e)}>
-                        <fieldset>
-                            <legend>Create a new chat room:</legend>
-                            <input placeholder="type a new room name" ref="roomName"/>
-                            <br /><br />
-                            <input type="submit" value="Create Room" />
-                        </fieldset>
-                    </form>
-                </div>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div>
+        <h1>Available Rooms:</h1>
+        {this.state.rooms.map((room, i) => (
+          <div>
+            <h4 className="room-name" key={i.toString()}>
+              {room.name}
+            </h4>
+          </div>
+        ))}
+        <div>
+          <form onSubmit={e => this.createRoom(e)}>
+            <fieldset>
+              <legend>Create a new chat room:</legend>
+              <input placeholder="type a new room name" ref="roomName" />
+              <br />
+              <br />
+              <input type="submit" value="Create Room" />
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    );
+  }
 } // class
 
 export default RoomList;
